403a5d6ed54b2735220f910fff4f4db410b7ebda
Vlad <vladnet@uw.edu>
2020-06-24 20:07:50 -0700

    Removed buffer inheritance to simplify code for embedded system

--- src/MicrocontrollerSupport/MicrocontrollerSupport/Buffer.h
+++ src/MicrocontrollerSupport/MicrocontrollerSupport/Buffer.h
@@ -1,32 +1,68 @@
 #pragma once
+#include "Util.h"
 
 template <class T>
 class Buffer {
-	public:
-		Buffer();
-		Buffer(const Buffer<T>& other);
-		virtual ~Buffer();
-		virtual const T* get(const int index);
-		virtual const T* peek();
-		virtual const T* pop();
-		virtual void push(const T& item);
-		virtual const T* remove(const int index);
-		int size() const;
-		bool isEmpty() const;
-		virtual void clear();
-	protected:
-		int numberOfItems;
+public:
+	Buffer();
+	Buffer(const Buffer<T>& other);
+	~Buffer();
+	const T* get(const int index);
+	const T* peek();
+	const T* remove(const int index);
+	const T* pop();
+	void push(const T& item);
+	int size() const;
+	bool isEmpty() const;
+	void clear();
+private:
+	// Node for bidirectional linked list
+	struct BufferNode {
+		BufferNode(const T& item) {
+			this->next = nullptr;
+			this->prev = nullptr;
+			this->item = new const T(item);
+		}
+		~BufferNode() {
+			delete this->item;
+		}
+		const T* item;
+		BufferNode* next;
+		BufferNode* prev;
+	};
+
+	int numberOfItems;
+
+	// Pointers to start and end of linked list
+	BufferNode* head;
+	BufferNode* tail;
+
+	// Keep track of last accessed index/node to improve nearby retrieval speeds
+	BufferNode* lastAccessed;
+	int lastAccessedIndex;
+
+	BufferNode* getPointer(const int index) const;
 };
 
 template <class T>
 Buffer<T>::Buffer() {
+	this->head = nullptr;
 	this->clear();
 }
 
 template <class T>
 Buffer<T>::Buffer(const Buffer<T>& other) {
-	this->clear();
-	this->numberOfItems = other.numberOfItems;
+	if (*this != other) {
+		this->clear();
+
+		int count = 0;
+		const T* item = other.get(count);
+		while (item != nullptr) {
+			this->push(item);
+			++count;
+			item = other.get(count);
+		}
+	}
 }
 
 template <class T>
@@ -35,32 +71,118 @@ Buffer<T>::~Buffer() {
 }
 
 template <class T>
-// Return item at given index
+// Returns pointer to item at target index, or nullptr if index is out of bounds or buffer is empty
 const T* Buffer<T>::get(const int index) {
-	return nullptr;
+	BufferNode* node = this->getPointer(index);
+	if (node == nullptr) {
+		return nullptr;
+	}
+
+	// Update last accessed pointer if desired node wasn't the head or tail
+	if (index != 0 || index != this->size() - 1) {
+		this->lastAccessed = node;
+		this->lastAccessedIndex = index;
+	}
+
+	return node->item;
 }
 
 template <class T>
-// Return first item
+// Returns pointer to first item in buffer, or nullptr if buffer is empty
 const T* Buffer<T>::peek() {
-	return nullptr;
+	return this->get(0);
 }
 
 template <class T>
-// Remove and return item at given index
+// Removes item at target index from buffer and returns pointer to it
 const T* Buffer<T>::remove(const int index) {
-	return nullptr;
+	const T* result = nullptr;
+
+	if (index == 0) {
+		if (this->head != nullptr) {
+			// Extract data from head
+			result = this->head->item;
+			this->head->item = nullptr;
+
+			// If last item was removed, put buffer in valid clear state
+			if (this->head->next == nullptr) {
+				this->clear();
+			}
+			else {
+				// Otherwise, just delete first node
+				BufferNode* temp = this->head->next;
+				temp->prev = nullptr;
+				delete this->head;
+				this->head = temp;
+				--Buffer<T>::numberOfItems;
+			}
+
+			// Don't modify last accessed, but keep pointing to the same node
+			--this->lastAccessedIndex;
+		}
+	}
+	else {
+		// Get pointer to node before the one to delete
+		BufferNode* node = this->getPointer(index - 1);
+		if (node != nullptr) {
+			BufferNode* toRemove = node->next;
+
+			// Extract data from the next buffer node
+			result = toRemove->item;
+			toRemove->item = nullptr;
+
+			// Update next/prev pointers
+			node->next = toRemove->next;
+			if (node->next != nullptr) {
+				node->next->prev = node;
+			}
+			else {
+				// If removing last item, update tail pointer
+				this->tail = node;
+			}
+
+			delete toRemove;
+			--Buffer<T>::numberOfItems;
+
+			if (this->lastAccessed != nullptr) {
+				// If deleted last accessed node, set last accessed to nullptr
+				if (this->lastAccessedIndex == index) {
+					this->lastAccessed = nullptr;
+				}
+				// If deleted earlier node, shift last accessed index left
+				else if (this->lastAccessedIndex > index) {
+					--this->lastAccessedIndex;
+				}
+			}
+		}
+	}
+
+	return result;
 }
 
 template <class T>
-// Remove and return first item
+// Removes first item from buffer and returns pointer to it
 const T* Buffer<T>::pop() {
-	return nullptr;
+	return this->remove(0);
 }
 
 template <class T>
-// Add item to end
-void Buffer<T>::push(const T& item) {}
+// Duplicates and appends item to the end of the buffer
+void Buffer<T>::push(const T& item) {
+	BufferNode* temp = new BufferNode(item);
+
+	if (this->isEmpty()) {
+		this->tail = temp;
+		this->head = this->tail;
+	}
+	else {
+		temp->prev = this->tail;
+		this->tail->next = temp;
+		this->tail = this->tail->next;
+	}
+
+	++Buffer<T>::numberOfItems;
+}
 
 template <class T>
 // Return size of buffer
@@ -75,7 +197,67 @@ bool Buffer<T>::isEmpty() const {
 }
 
 template <class T>
-// Clear buffer
+// Deletes all items in buffer and sets class members to empty state
 void Buffer<T>::clear() {
-	this->numberOfItems = 0;
+	// Delete all items in linked list
+	BufferNode* temp;
+	while (this->head != nullptr) {
+		temp = this->head->next;
+		delete this->head;
+		this->head = temp;
+	}
+
+	// Update all pointers and counts
+	this->tail = nullptr;
+	this->lastAccessed = nullptr;
+	this->lastAccessedIndex = 0;
+	Buffer<T>::numberOfItems = 0;
+}
+
+template <class T>
+// Returns pointer to node at provided index, or nullptr if index is out of bounds. Utilizes
+// additional pointer to last accessed item to improve lookup efficiency of nearby indeces.
+typename Buffer<T>::BufferNode* Buffer<T>::getPointer(const int index) const {
+	const int size = this->size();
+
+	if (this->isEmpty() || index < 0 || index >= size) {
+		return nullptr;
+	}
+	else if (index == 0) {
+		return this->head;
+	}
+	else if (index == size - 1) {
+		return this->tail;
+	}
+	else {
+		// Calculate distances from all three pointers
+		const int distFromLast = this->lastAccessed == nullptr ? size : Util::Math::absV(this->lastAccessedIndex - index);
+		const int distFromHead = index - 0;
+		const int distFromTail = size - index - 1;
+
+		BufferNode* closest;
+
+		// Find shortest distance, copy closest pointer, and determine direction it should move along buffer (true = towards next, false = towards previous)
+		const int closestDist = Util::Math::minV(distFromLast, Util::Math::minV(distFromHead, distFromTail));
+		bool moveTowardsNext;
+		if (closestDist == distFromLast) {
+			closest = this->lastAccessed;
+			moveTowardsNext = index - lastAccessedIndex >= 0;
+		}
+		else if (closestDist == distFromHead) {
+			closest = this->head;
+			moveTowardsNext = true;
+		}
+		else {
+			closest = this->tail;
+			moveTowardsNext = false;
+		}
+
+		// Navigate pointer to index
+		for (int i = 0; i < closestDist; ++i) {
+			closest = moveTowardsNext ? closest->next : closest->prev;
+		}
+
+		return closest;
+	}
 }
